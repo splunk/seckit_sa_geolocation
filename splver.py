b60e7c6669301bc1d3e34486e186b5a2f77e3258
rfaircloth-splunk <rfaircloth@splunk.com>
2020-02-18 10:17:55 -0500

    splunk ver script

---
+++ splver.py
@@ -0,0 +1,15 @@
+from __future__ import print_function
+import sys
+
+import re
+
+regex = r'v([0-9]+\.[0-9]+\.[0-9]+)(?:-([abr][a-z]*|d).*(?:\+|\.)(.*))?'
+
+test_str = sys.argv[1]
+
+subst = "\\1\\2\\3"
+
+result = re.sub(regex, subst, test_str, 0, re.MULTILINE)
+
+if result:
+    print(result)
