ce1f0aa82621997e2fd2ec7086368fcc6efd7d4a
rfaircloth-splunk <rfaircloth@splunk.com>
2020-09-19 17:52:44 -0400

    Update test_addon.py

--- tests/test_addon.py
+++ tests/test_addon.py
@@ -8,6 +8,6 @@ import pytest
 from pytest_splunk_addon.standard_lib.addon_basic import Basic
 
 
-class Test_App(Basic):
-    def empty_method(self):
-        pass
+# class Test_App(Basic):
+#     def empty_method(self):
+#         pass
