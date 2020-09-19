3d41f685113294437f944eb4200061117a3575c6
rfaircloth-splunk <rfaircloth@splunk.com>
2020-09-19 17:36:20 -0400

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,26 @@
+import os
 import pytest
+
+
+def pytest_configure(config):
+    config.addinivalue_line("markers", "external: Test search time only")
+    config.addinivalue_line("markers", "docker: Test search time only")
+
+
+@pytest.fixture(scope="session")
+def docker_compose_files(request):
+    """
+    Get an absolute path to the  `docker-compose.yml` file. Override this
+    fixture in your tests if you need a custom location.
+    Returns:
+        string: the path of the `docker-compose.yml` file
+    """
+    docker_compose_path = os.path.join(
+        str(request.config.invocation_dir), "docker-compose.yml"
+    )
+    # LOGGER.info("docker-compose path: %s", docker_compose_path)
+
+    return [docker_compose_path]
 
 
 def pytest_addoption(parser):
@@ -21,17 +43,23 @@ def pytest_addoption(parser):
 
 
 @pytest.fixture(scope="session")
-def seckit_input(splunk_rest, request):
+def seckit_input(splunk_rest_uri, request):
 
     payload = {
-        'name': 'main',
-        'output_mode': 'json',
-        'username': request.config.getoption("maxmind_id"),
-        'password': request.config.getoption("maxmind_password")
+        "name": "main",
+        "output_mode": "json",
+        "username": request.config.getoption("maxmind_id"),
+        "password": request.config.getoption("maxmind_password"),
     }
 
-    splunk_rest[0].post(f'{splunk_rest[1]}servicesNS/nobody/SecKit_SA_geolocation/SecKit_SA_geolocation_account/create',
-                        data=payload, verify=False).status_code
+    splunk_rest_uri[0].post(
+        f"{splunk_rest[1]}servicesNS/nobody/SecKit_SA_geolocation/SecKit_SA_geolocation_account/create",
+        data=payload,
+        verify=False,
+    ).status_code
 
-    splunk_rest[0].post(f'{splunk_rest[1]}servicesNS/nobody/SecKit_SA_geolocation/SecKit_SA_geolocation_geoipupdate/main?output_mode=json',
-                        data={'disabled': '0'}, verify=False).status_code
+    splunk_rest_uri[0].post(
+        f"{splunk_rest[1]}servicesNS/nobody/SecKit_SA_geolocation/SecKit_SA_geolocation_geoipupdate/main?output_mode=json",
+        data={"disabled": "0"},
+        verify=False,
+    ).status_code
