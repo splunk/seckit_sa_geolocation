14bc29fbb112484b808dc2af8ca7efc1999b4298
Addon Factory template <addonfactory@splunk.com>
2020-11-11 16:32:36 +0000

    sync for policy

--- tests/entrypoint.sh
+++ tests/entrypoint.sh
@@ -6,4 +6,42 @@
 ##
 
 cd /home/circleci/work
-pytest $@
+if [ -f "${TEST_SET}/pytest-ci.ini" ]; then
+    cp -f ${TEST_SET}/pytest-ci.ini pytest.ini
+fi
+
+pip install -r requirements_dev.txt
+pip install six>=1.15
+pip install git+https://github.com/rfaircloth-splunk/agent-python-pytest.git
+
+cp -f .pytest.expect ${TEST_SET}
+
+echo "Executing Tests..."
+RERUN_COUNT=${RERUN_COUNT:-1}
+if [ -z ${TEST_BROWSER} ] 
+then
+    echo Test Args $@ ${TEST_SET}
+    pytest $@ ${TEST_SET}
+    test_exit_code=$?
+else
+    # Execute the tests on Headless mode in local if UI_TEST_HEADLESS environment is set to "true"
+    if [ "${UI_TEST_HEADLESS}" = "true" ]
+    then
+        echo Test Args $@ --local --persist-browser --headless --reruns=${RERUN_COUNT} --browser=${TEST_BROWSER} ${TEST_SET}
+        pytest $@ --local --persist-browser --headless --reruns=${RERUN_COUNT} --browser=${TEST_BROWSER}
+        --reportportal -o "rp_endpoint=${RP_ENDPOINT}" -o "rp_launch_attributes=${RP_LAUNCH_ATTRIBUTES}" \
+        -o "rp_project=${RP_PROJECT}" -o "rp_launch=${RP_LAUNCH}" -o "rp_launch_description='${RP_LAUNCH_DESC}'" -o "rp_ignore_attributes='xfail' 'usefixture'" \
+        ${TEST_SET}
+        test_exit_code=$?
+    else
+        echo "Check Saucelab connection..."
+        wget --retry-connrefused --no-check-certificate -T 10 sauceconnect:4445 
+        echo Test Args $@ --reruns=${RERUN_COUNT} --browser=${TEST_BROWSER} ${TEST_SET}
+        pytest $@ --reruns=${RERUN_COUNT} --browser=${TEST_BROWSER} \
+        --reportportal -o "rp_endpoint=${RP_ENDPOINT}" -o "rp_launch_attributes=${RP_LAUNCH_ATTRIBUTES}" \
+        -o "rp_project=${RP_PROJECT}" -o "rp_launch=${RP_LAUNCH}" -o "rp_launch_description='${RP_LAUNCH_DESC}'" -o "rp_ignore_attributes='xfail' 'usefixture'" \
+        ${TEST_SET}
+        test_exit_code=$?
+    fi
+fi
+exit "$test_exit_code" 
