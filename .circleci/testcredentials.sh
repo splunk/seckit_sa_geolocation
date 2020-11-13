8532daa4f180564fae55a40560a7bc5ec170d653
rfaircloth-splunk <rfaircloth@splunk.com>
2020-11-12 19:20:47 -0500

    Update testcredentials.sh

--- .circleci/testcredentials.sh
+++ .circleci/testcredentials.sh
@@ -1,2 +1,2 @@
-      echo MAXMIND_ACCOUNT=${MAXMIND_ACCOUNT} >> testcredentials.env
-      echo MAXMIND_KEY=${MAXMIND_KEY} >> testcredentials.env
+      echo MAXMIND_ACCOUNT=${MAXMIND_ACCOUNT} >> test_credentials.env
+      echo MAXMIND_KEY=${MAXMIND_KEY} >> test_credentials.env
