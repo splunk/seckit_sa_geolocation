394128b1a4cfda07d15533b460ae4c4a595cbb28
rfaircloth-splunk <rfaircloth@splunk.com>
2020-11-13 07:36:27 -0500

    Fix error where a non ip value is present in the input

--- tests/knowledge/test_common.py
+++ tests/knowledge/test_common.py
@@ -6,9 +6,19 @@
 
 
 
-def test_isp(record_property, splunk_search_util,seckit_input):
+def test_positive(record_property, splunk_search_util,seckit_input):
 
     search = "| makeresults | eval src=\"8.8.8.8\" | `seckit_iplocation(src,src)` | search src_country=US"
+
+    result = splunk_search_util.checkQueryCountIsGreaterThanZero(
+        search, interval=20, retries=20
+    )
+
+    assert result
+
+def test_negative(record_property, splunk_search_util,seckit_input):
+    
+    search = "| makeresults | eval src=\"example.com\" | `seckit_iplocation(src,src)` | search NOT src_lat=*"
 
     result = splunk_search_util.checkQueryCountIsGreaterThanZero(
         search, interval=20, retries=20
