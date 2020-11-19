81cc5b853938a9823b071d7a6efa22f335d1df4e
Addon Factory template <addonfactory@splunk.com>
2020-11-19 15:37:50 +0000

    sync for policy

--- tests/entrypoint.sh
+++ tests/entrypoint.sh
@@ -20,26 +20,17 @@ echo "Executing Tests..."
 RERUN_COUNT=${RERUN_COUNT:-1}
 if [ -z ${TEST_BROWSER} ] 
 then
-    echo Test Args $@ --reportportal -o "rp_endpoint=${RP_ENDPOINT}" -o "rp_launch_attributes=${RP_LAUNCH_ATTRIBUTES}" \
-    -o "rp_project=${RP_PROJECT}" -o "rp_launch=${RP_LAUNCH}" -o "rp_launch_description='${RP_LAUNCH_DESC}'" -o "rp_ignore_attributes='xfail' 'usefixture'" \
-    ${TEST_SET}     
-    pytest $@ \
-    --reportportal -o "rp_endpoint=${RP_ENDPOINT}" -o "rp_launch_attributes=${RP_LAUNCH_ATTRIBUTES}" \
-    -o "rp_project=${RP_PROJECT}" -o "rp_launch=${RP_LAUNCH}" -o "rp_launch_description='${RP_LAUNCH_DESC}'" -o "rp_ignore_attributes='xfail' 'usefixture'" \
-    ${TEST_SET} 
+    echo Test Args $@ ${TEST_SET}
+    pytest $@ ${TEST_SET}
     test_exit_code=$?
 else
     # Execute the tests on Headless mode in local if UI_TEST_HEADLESS environment is set to "true"
     if [ "${UI_TEST_HEADLESS}" = "true" ]
     then
         echo Test Args $@ --local --persist-browser --headless --reruns=${RERUN_COUNT} --browser=${TEST_BROWSER}\
-        --reportportal -o "rp_endpoint=${RP_ENDPOINT}" -o "rp_launch_attributes=${RP_LAUNCH_ATTRIBUTES}" \
-        -o "rp_project=${RP_PROJECT}" -o "rp_launch=${RP_LAUNCH}" -o "rp_launch_description='${RP_LAUNCH_DESC}'" -o "rp_ignore_attributes='xfail' 'usefixture'" \
         ${TEST_SET} 
         
         pytest $@ --local --persist-browser --headless --reruns=${RERUN_COUNT} --browser=${TEST_BROWSER} \
-        --reportportal -o "rp_endpoint=${RP_ENDPOINT}" -o "rp_launch_attributes=${RP_LAUNCH_ATTRIBUTES}" \
-        -o "rp_project=${RP_PROJECT}" -o "rp_launch=${RP_LAUNCH}" -o "rp_launch_description='${RP_LAUNCH_DESC}'" -o "rp_ignore_attributes='xfail' 'usefixture'" \
         ${TEST_SET}
         test_exit_code=$?
     else
@@ -50,8 +41,6 @@ else
         [ "$sauce_connect_connection" -eq "4" ] && echo "SauceConnect is not running. Exiting the tests...." && exit 1
         echo Test Args $@ --reruns=${RERUN_COUNT} --browser=${TEST_BROWSER} ${TEST_SET}
         pytest $@ --reruns=${RERUN_COUNT} --browser=${TEST_BROWSER} \
-        --reportportal -o "rp_endpoint=${RP_ENDPOINT}" -o "rp_launch_attributes=${RP_LAUNCH_ATTRIBUTES}" \
-        -o "rp_project=${RP_PROJECT}" -o "rp_launch=${RP_LAUNCH}" -o "rp_launch_description='${RP_LAUNCH_DESC}'" -o "rp_ignore_attributes='xfail' 'usefixture'" \
         ${TEST_SET}
         test_exit_code=$?
     fi
