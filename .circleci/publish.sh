529eae9804261c2b858ca0145144e41129f5a461
rfaircloth-splunk <rfaircloth@splunk.com>
2021-02-13 19:59:49 -0500

    chore: update license

--- .circleci/publish.sh
+++ .circleci/publish.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 ##
 ## SPDX-FileCopyrightText: 2020 Splunk, Inc. <sales@splunk.com>
-## SPDX-License-Identifier: LicenseRef-Apache-2.0
+## SPDX-License-Identifier: Apache-2.0
 ##
 ##
 source .splunkbase 
