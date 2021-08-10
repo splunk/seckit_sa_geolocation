751b835dfbe6ae796345ba4d738c27a523bd9dc3
Ryan Faircloth <rfaircloth@splunk.com>
2021-08-10 18:09:46 -0400

    feat: Support SHC (#120)
    This feature adds support for SHC using an in-development feature of Splunk Enterprise/Cloud that is disabled by default. ```[global]/allowRestReplay=true``` must be enabled per Splunk Documentation as of Splunk 8.2 "Do not enable it without consulting Splunk support." Do not use with versions prior to 8.2

--- tests/knowledge/test_common.py
+++ tests/knowledge/test_common.py
@@ -5,7 +5,7 @@
 
 def test_positive(record_property, splunk_search_util,seckit_input):
 
-    search = "| makeresults | eval src=\"8.8.8.8\" | `seckit_iplocation(src,src)` | search src_country=US"
+    search = '| makeresults | eval src="8.8.8.8" | `seckit_iplocation(src,src)` | search src_country=US'
 
     result = splunk_search_util.checkQueryCountIsGreaterThanZero(
         search, interval=20, retries=20
@@ -15,7 +15,7 @@ def test_positive(record_property, splunk_search_util,seckit_input):
 
 def test_negative(record_property, splunk_search_util,seckit_input):
     
-    search = "| makeresults | eval src=\"example.com\" | `seckit_iplocation(src,src)` | search NOT src_lat=*"
+    search = '| makeresults | eval src="example.com" | `seckit_iplocation(src,src)` | search NOT src_lat=*'
 
     result = splunk_search_util.checkQueryCountIsGreaterThanZero(
         search, interval=20, retries=20
@@ -28,7 +28,7 @@ def test_update(record_property, splunk_search_util, seckit_input):
 
 
 
-    search = 'search index=_internal source="*SecKit_SA_geolocation_input.log" mmdb=GeoLite2-City.mmdb size=*'
+    search = 'search index=_internal source="*/SecKit_SA_geolocation_rh_updater.log" mmdb=GeoLite2-City.mmdb size=*'
 
     result = splunk_search_util.checkQueryCountIsGreaterThanZero(
         search, interval=20, retries=20
