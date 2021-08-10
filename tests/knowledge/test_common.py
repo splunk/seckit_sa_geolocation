8caab1cb5f197da0c176a1b160c877a6ce65da51
rfaircloth-splunk <rfaircloth@splunk.com>
2021-08-10 16:30:29 -0400

    Update test_common.py

--- tests/knowledge/test_common.py
+++ tests/knowledge/test_common.py
@@ -28,7 +28,7 @@ def test_update(record_property, splunk_search_util, seckit_input):
 
 
 
-    search = 'search index=_internal source="*SecKit_SA_geolocation_input.log" mmdb=GeoLite2-City.mmdb size=*'
+    search = 'search index=_internal source="*SecKit_SA_geolocation_handler.log" mmdb=GeoLite2-City.mmdb size=*'
 
     result = splunk_search_util.checkQueryCountIsGreaterThanZero(
         search, interval=20, retries=20
