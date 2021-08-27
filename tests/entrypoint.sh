acdff9eebec8a239fb27b00c284879c565dbae0f
Ryan Faircloth <rfaircloth@splunk.com>
2021-08-27 08:13:13 -0400

    test: common template rollout changes (#123)
    Co-authored-by: Addon Factory template <addonfactory@splunk.com>

--- tests/entrypoint.sh
+++ tests/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ##
 ## SPDX-FileCopyrightText: 2021 Splunk, Inc. <sales@splunk.com>
-## SPDX-License-Identifier: LicenseRef-Splunk-1-2020
+## SPDX-License-Identifier: LicenseRef-Splunk-8-2021
 ##
 ##
 # shellcheck disable=SC2164,SC2086,SC2068
